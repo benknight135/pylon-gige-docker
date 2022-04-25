e13c10a32b18bae8722d0deba8e2167443430037
Ben Knight <benknight135@gmail.com>
2022-04-25 15:16:44 +0100

    update messages in run script

--- app/run.py
+++ app/run.py
@@ -5,14 +5,21 @@ tl_factory = pylon.TlFactory.GetInstance()
 
 # Get connected devices
 devices = tl_factory.EnumerateDevices()
-# List connected GigE devices
+if (len(devices) <= 0):
+    raise EnvironmentError("no devices found")
+# List connected devices
+gige_devices = []
 for device in devices:
     # Only print info on GigE devices
     if device.GetDeviceClass() == 'BaslerGigE':
-        print(
-            "Device %s @ %s (%s)" % (
-                device.GetModelName(),
-                device.GetIpAddress(),
-                device.GetMacAddress()))
-else:
+        gige_devices.append(device)
+# Check a GigE device was found
+if (len(gige_devices) <= 0):
     raise EnvironmentError("no GigE devices found")
+# Print connected GigE devices
+for device in gige_devices:
+    print(
+        "Device %s @ %s (%s)" % (
+            device.GetModelName(),
+            device.GetIpAddress(),
+            device.GetMacAddress()))
